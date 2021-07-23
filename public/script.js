62da5a7ed4cdcc499bcfee5405cfe315106bda61
Ricky Ho <horicky78@gmail.com>
2021-07-23 17:21:58 +0000

    Added pages, css, and js files

---
+++ public/script.js
@@ -0,0 +1,14 @@
+const submit = document.querySelector("#submit")
+ const personList = {
+
+    }
+
+
+const btn = document.querySelector("button"); // Get the button from the page
+// Detect clicks on the button
+if (btn) {
+  btn.onclick = function() {
+    // The JS works in conjunction with the 'dipped' code in style.css
+    btn.classList.toggle("dipped");
+  };
+}
